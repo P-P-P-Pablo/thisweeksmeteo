f32357540cd84ba758b62446921c96aa75db888b
P-P-P-Pablo <pvilella.pro@gmail.com>
2020-12-06 12:00:39 +0100

    saving progress ...

--- src/components/Day.vue
+++ src/components/Day.vue
@@ -7,37 +7,40 @@
 					: 'J +' + meteoday.day + ' : ' + parseDate(meteoday.datetime)
 			}}
 		</h2>
-        <div class="card-front">
-            <img :src="pictoUrl" :alt="parseWeather(meteoday.weather)" />
-        </div>
-        <div class= "card-back">
-		<p>Prévisions : {{ parseWeather(meteoday.weather) }}</p>
-		<p>Probabilité de pluie : {{ meteoday.probarain }}%</p>
-		<p>Temperature min/max : {{ meteoday.tmin }}°C/{{ meteoday.tmax }}°C</p>
-		<p>Vent : {{ meteoday.wind10m }}km/h</p>
-        </div>
+		<div class="card-front">
+			<img
+				class="picto"
+				:src="pictoFetcher(this.meteoday.weather)"
+				:alt="parseWeather(meteoday.weather)"
+			/>
+		</div>
+		<div class="card-back">
+			<p>Prévisions : {{ parseWeather(meteoday.weather) }}</p>
+			<p>Probabilité de pluie : {{ meteoday.probarain }}%</p>
+			<p>Temperature min/max : {{ meteoday.tmin }}°C/{{ meteoday.tmax }}°C</p>
+			<p>Vent : {{ meteoday.wind10m }}km/h</p>
+		</div>
 	</li>
 </template>
 
 <script>
 import dayjs from 'dayjs';
 import codetemps from '../assets/codetemps.json';
-import fetchPicto from "../utils"
+import fetchPicto from '../utils';
 export default {
-    name:"Day",
-    props: ['meteoday'],
-    data(){
-        pictoUrl: fetchPicto(this.meteoday.weather)
-    },
-    methods: {
+	name: 'Day',
+	props: ['meteoday'],
+	methods: {
 		parseDate(date) {
 			return dayjs(date).format('DD/MM/YYYY');
 		},
 		parseWeather(code) {
 			return codetemps[code];
-		},
+        },
+        pictoFetcher(code){
+            return fetchPicto(code)
+        }
 	},
-	
 };
 </script>
 
@@ -49,5 +52,9 @@ export default {
 	height: auto;
 	border: solid 2px darkmagenta;
 	border-radius: 10px;
+}
+.picto {
+	width: 50%;
+	height: 50%;
 }
 </style>
